b5090242583fcc2b06cd6398554d3259b0a33bda
Midokhalil1 <midokhalil22@gmail.com>
2022-11-09 16:25:23 -0500

    updeted frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AlbumList from './AlbumList';
+
+// I'm going to fetch http://localhost:5002/albums
+// and display them 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AlbumList />
+      <Footer />
+      
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2022 Mido Khalil.
+                Source: <a href="http://localhost:5002/albums"></a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer 
